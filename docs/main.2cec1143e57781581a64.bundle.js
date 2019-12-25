0f51e95220033e25155a18b6aa294df54ff28873
Kwame Opare Asiedu <kwasiedu.5@gmail.com>
2019-12-25 02:41:47 +0000

    Built storybook into docs folder. Updated dev dependencies

---
+++ docs/main.2cec1143e57781581a64.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,c){c(397),c(536),n.exports=c(825)},1:function(n,o){},536:function(n,o,c){"use strict";c.r(o);c(537),c(822)}},[[0,1,2]]]);
